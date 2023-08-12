78385d48425232bcb374b927683397c2db810f70
Marina Vasileva <mv031a@yandex.ru>
2023-08-12 21:01:15 +0400

    feat: Добавить файлы utils rest api

---
+++ src/utils/MainApi.js
@@ -0,0 +1,114 @@
+import { BASE_API_URL, MOVIES_API_URL } from "./constants";
+
+class MainApi {
+  constructor(setting) {
+    this._baseUrl = setting.baseUrl;
+    this._headers = setting.headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  register(data) {
+    return fetch(`${this._baseUrl}/signup`, {
+      method: "POST",
+      headers: this._headers,
+      credentials: "include",
+      body: JSON.stringify({
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  login(data) {
+    return fetch(`${this._baseUrl}/signin`, {
+      method: "POST",
+      credentials: "include",
+      headers: this._headers,
+      body: JSON.stringify({
+        email: data.email,
+        password: data.password,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  logout() {
+    return fetch(`${this._baseUrl}/signout`, {
+      method: "POST",
+      headers: this._headers,
+      credentials: "include",
+    }).then(this._checkResponse);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "GET",
+      headers: this._headers,
+      credentials: "include",
+    }).then(this._checkResponse);
+  }
+
+  editUserInfo(data) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      credentials: "include",
+      body: JSON.stringify({
+        name: data.name,
+        email: data.email,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  saveMovie(data) {
+    return fetch(`${this._baseUrl}/movies`, {
+      method: "POST",
+      headers: this._headers,
+      credentials: "include",
+      body: JSON.stringify({
+        country: data.country,
+        director: data.director,
+        duration: data.duration,
+        year: data.year,
+        description: data.description,
+        image: MOVIES_API_URL + data.image.url,
+        trailerLink: data.trailerLink,
+        thumbnail: MOVIES_API_URL + data.image.formats.thumbnail.url,
+        movieId: data.id,
+        nameRU: data.nameRU,
+        nameEN: data.nameEN,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  deleteMovie(movieId) {
+    return fetch(`${this._baseUrl}/movies/${movieId}`, {
+      method: "DELETE",
+      headers: this._headers,
+      credentials: "include",
+    }).then(this._checkResponse);
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/movies`, {
+      method: "GET",
+      headers: this._headers,
+      credentials: "include",
+    }).then(this._checkResponse);
+  }
+}
+
+const mainApi = new MainApi({
+  baseUrl: BASE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default mainApi;
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+const BASE_API_URL = 'https://api.miimesto.nomoredomains.rocks/';
+const MOVIES_API_URL = 'https://api.nomoreparties.co';
+
+export {
+    BASE_API_URL,
+    MOVIES_API_URL
+};
+
